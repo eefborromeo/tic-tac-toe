4931f10aafe34a107f9570bf8de9638e667423ff
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-02-27 17:37:52 +0800

    updates player status on the board

--- index.js
+++ index.js
@@ -19,12 +19,21 @@ let board = [
     ['', '', ''],
 ]
 
+function updatesPlayerStatus() {
+   statusEl.textContent = `${currentPlayer === x.name ? o.name : x.name}'s turn`;
+   const boxesArr = Array.prototype.slice.call(boxes)
+    if (boxesArr.every(box => box.disabled)) {
+        statusEl.textContent = `Game Over`;
+    }
+}
+
 function addMove() {
     currentPlayer = currentPlayer === x.name ? o.name : x.name;
     board[this.dataset.boardIndex].splice(this.dataset.index, 1, currentPlayer);
     currentPlayer === x.name ? this.classList.add('x') : this.classList.add('o')
     this.textContent = board[this.dataset.boardIndex][this.dataset.index];
     this.disabled = true;
+    updatesPlayerStatus();
 }
 
 boxes.forEach(box => {
