c9e95c3a5c41fb34d1c9880dd25819af6785484d
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-02-28 20:35:33 +0800

    separate win validation into separate functions

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ function addMove() {
     this.textContent = board[this.dataset.boardIndex][this.dataset.index];
     this.disabled = true;
     updatesPlayerStatus();
+    checkHorizontalWin();
+    checkVerticalWin();
+    checkDiagonalWin();
     checkWin();
 }
 
@@ -37,7 +40,7 @@ function disableBoard() {
     boxes.forEach(box => box.disabled = 'true');
 }
 
-function checkWin() {
+function checkHorizontalWin() {
     // HORIZONTAL WIN
     const row1 = board[0];
     const row2 = board[1];
@@ -49,43 +52,57 @@ function checkWin() {
     
     if (row1Check && !row2Check && !row3Check) {
         statusEl.textContent = `Game Over. ${row1[0]} wins!`;
-        disableBoard();
+        return true
     } else if (!row1Check && row2Check && !row3Check) {
         statusEl.textContent = `Game Over. ${row2[0]} wins!`;
-        disableBoard();
+        return true
     } else if (!row1Check && !row2Check && row3Check) {
         statusEl.textContent = `Game Over. ${row3[0]} wins!`;
-        disableBoard();
-    } 
-            
+        return true
+    }
+}
+
+function checkVerticalWin() {
     // VERTICAL WIN
     const col1 = board.map(row => row[0]);
     const col2 = board.map(row => row[1]);
     const col3 = board.map(row => row[2]);
-
+    
     const col1Check = col1.every(char => char === col1[0] && char !== '');
     const col2Check = col2.every(char => char === col2[0] && char !== '');
     const col3Check = col3.every(char => char === col3[0] && char !== '');
-
+    
     if (col1Check && !col2Check && !col3Check) {
         statusEl.textContent = `Game Over. ${col1[0]} wins!`;
-        disableBoard();
+        return true
     } else if (!col1Check && col2Check && !col3Check) {
         statusEl.textContent = `Game Over. ${col2[0]} wins!`;
-        disableBoard();
+        return true
     } else if (!col1Check && !col2Check && col3Check) {
         statusEl.textContent = `Game Over. ${col3[0]} wins!`;
-        disableBoard();
+        return true
     }
+}
 
+function checkDiagonalWin() {
     // DIAGONAL WIN
     const leftRight = board.map((row, index) => row[index]);
     const leftRightCheck = leftRight.every(char => char === leftRight[0] && char !== '');
     const rightLeft = board.map((row, index) => row.reverse()[index]);
     const rightLeftCheck = rightLeft.every(char => char === rightLeft[0] && char !== '');
-
+    
     if (leftRightCheck || rightLeftCheck) {
         statusEl.textContent = `Game Over. ${leftRightCheck ?leftRight[0] : rightLeft[0]} wins!`;
+        return true
+    } 
+}
+
+function checkWin() {
+    const boxesArr = Array.from(boxes);
+    const noEmptyBox = boxesArr.every(box => box.disabled);
+    if ((!checkHorizontalWin() || !checkVerticalWin() || !checkDiagonalWin()) && noEmptyBox) {
+        statusEl.textContent = `Draw! Game Over.`;
+    } else if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) {
         disableBoard();
     }
 }
