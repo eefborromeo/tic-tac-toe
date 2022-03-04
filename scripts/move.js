7d22d920b0cbc8e5b08f552d2afc854ceaef0cf8
Eloisa <27989510+eefborromeo@users.noreply.github.com>
2022-03-04 11:21:13 +0800

    added history of moves feature

--- scripts/move.js
+++ scripts/move.js
@@ -1,32 +1,63 @@
-import { boxes, x, o, board, statusEl, questionDiv } from './variables.js';
-import { checkHorizontalWin, checkVerticalWin, checkDiagonalWin, checkWin } from './win.js';
+import { boxes, x, o, board, statusEl, state, questionDiv } from './variables.js';
+import { checkWin } from './win.js';
 import { addScore } from './score.js';
-import { showHistory } from './history.js'
+import { counter, previous, updatePrevious, showHistory, resetCounter } from './history.js'
 
 const playerChoice = document.querySelectorAll('[data-player-name]');
+const moveHistory = document.querySelector('#moves > ol');
+
+let literalMoves = [
+    ['top left', 'top center', 'top right'],
+    ['center left', 'center center', 'center right'],
+    ['bottom left', 'bottom center', 'bottom right'],
+]
 
 export let currentPlayer;
 
 export let moves = [];
 
+export let presentMove;
+
 export function toggleCurrentPlayer() {
-    return currentPlayer = currentPlayer === x.name ? o.name : x.name;
+    currentPlayer = currentPlayer === x.name ? o.name : x.name;
+    statusEl.textContent = `${currentPlayer}'s turn`;
+    return currentPlayer;
+}
+
+export function updateMove(val) {
+    moves = val;
+}
+
+export function showBoard() {
+    boxes.forEach(box => {
+        box.textContent = board[box.dataset.boardIndex][box.dataset.index];
+        if (box.textContent !== '') {
+            box.textContent === x.name ? box.classList.add('x') : box.classList.add('o');
+        } else if (box.textContent === '') {
+            box.classList.remove('x'); 
+            box.classList.remove('o');
+        }
+    })
 }
 
 function addMove() {
-    currentPlayer === x.name ? 
-    (this.textContent = x.name, this.classList.add('x'), board[this.dataset.boardIndex][this.dataset.index] = currentPlayer) : 
-    (this.textContent = o.name, this.classList.add('o'), board[this.dataset.boardIndex][this.dataset.index] = currentPlayer);
-    toggleCurrentPlayer();
-    statusEl.textContent = `${currentPlayer}'s turn`;
+    board[this.dataset.boardIndex][this.dataset.index] = currentPlayer;
     this.disabled = true;
-    checkHorizontalWin();
-    checkVerticalWin();
-    checkDiagonalWin();
+    moves.unshift(JSON.parse(JSON.stringify(board)));
+    // state.present = moves[counter];
+    // state.past.unshift(JSON.parse(JSON.stringify(state.present)));
+    // state.past.unshift(JSON.parse(JSON.stringify(board)));
+    // state.past = moves.slice();
+    // state.present = state.past.shift();
+    resetCounter();
+    toggleCurrentPlayer();
     checkWin();
     addScore();
-    showHistory();
-    moves.push(JSON.parse(JSON.stringify(board)))
+    showBoard();
+    // add move to history
+    const li = document.createElement('li');
+    li.textContent = `${currentPlayer} played ${literalMoves[this.dataset.boardIndex][this.dataset.index]}`
+    moveHistory.appendChild(li);
 }
 
 function addCurrentPlayer() {
